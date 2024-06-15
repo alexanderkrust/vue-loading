b5ed9c773c884d78edc0cdc56d9b91fcf312a340
Alexander Rust <work@alexanderrust.de>
2024-06-15 11:03:23 +0200

    refactor(moon-loader): fixed wobble animation

--- packages/vue-loading/src/moon-loader.vue
+++ packages/vue-loading/src/moon-loader.vue
@@ -1,99 +1,81 @@
-<script lang="ts" setup>
-import type { StyleValue } from 'vue'
-import { computed } from 'vue'
-import { cn } from './utils/cn'
+<script lang="ts">
 import type { LoaderPrimitiveProps } from './types'
-import { BASE_PRIMARY_COLOR, BASE_SIZE, BASE_SPEED } from './const'
+import type { StyleValue } from 'vue'
 
 export interface MoonLoaderProps extends LoaderPrimitiveProps {
   color?: string
   size?: number
-  radius?: string | number
   speed?: number
 }
+</script>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { cn } from './utils/cn'
+
+import { BASE_PRIMARY_COLOR, BASE_SIZE, BASE_SPEED } from './const'
 
 const props = withDefaults(defineProps<MoonLoaderProps>(), {
   color: BASE_PRIMARY_COLOR,
   size: BASE_SIZE,
-  radius: '100%',
   speed: BASE_SPEED,
 })
 
-const moonSize = computed(() => props.size / 7)
+const moonSize = computed(() => Math.floor(props.size / 7))
 
-function sharedStyles(width: number): Record<string, string> {
+const wrapper = computed<StyleValue>(() => ({
+  'display': 'inherit',
+  'position': 'relative',
+  'width': `${`${Math.floor(props.size + moonSize.value * 2)}px`}`,
+  'height': `${`${Math.floor(props.size + moonSize.value * 2)}px`}`,
+  'animation': `vl-moon-rotation ${0.6 / props.speed}s 0s infinite linear`,
+  '-webkit-animation': `vl-moon-rotation ${0.6 / props.speed}s 0s infinite linear`,
+  'animationFillMode': 'forwards',
+}))
+
+function ballStyles(size: number) {
   return {
-    width: `${width}px`,
-    height: `${width}px`,
+    width: `${size}px`,
+    height: `${size}px`,
     borderRadius: '100%',
-  }
+  } satisfies StyleValue
 }
 
-const wrapperStyles = computed(() => {
-  return {
-    display: 'inherit',
-    position: 'relative',
-    width: `${`${props.size + moonSize.value * 2}px`}`,
-    height: `${`${props.size + moonSize.value * 2}px`}`,
-  } as StyleValue
-})
+const ball = computed<StyleValue>(() => ({
+  ...ballStyles(moonSize.value),
+  'backgroundColor': `${props.color}`,
+  'opacity': '0.8',
+  'position': 'absolute',
+  'top': `${`${props.size / 2 - moonSize.value / 2}px`}`,
+  'animation': `vl-moon-rotation ${0.6 / props.speed}s 0s infinite linear`,
+  '-webkit-animation': `vl-moon-rotation ${0.6 / props.speed}s 0s infinite linear`,
+}))
 
-const ballStyles = computed(() => {
-  return {
-    ...sharedStyles(moonSize.value),
-    backgroundColor: props.color,
-    opacity: 0.8,
-    position: 'absolute',
-    top: `${props.size / 2 - moonSize.value / 2}px`,
-  } satisfies StyleValue
-})
-
-const circleStyles = computed(() => {
-  return {
-    ...sharedStyles(props.size),
-    border: `${moonSize.value}px solid ${props.color}`,
-    opacity: 0.1,
-    boxSizing: 'content-box',
-    position: 'absolute',
-  } satisfies StyleValue
-})
+const circle = computed<StyleValue>(() => ({
+  ...ballStyles(props.size),
+  border: `${moonSize.value}px solid ${props.color}`,
+  opacity: '0.1',
+  boxSizing: 'content-box',
+  position: 'absolute',
+}))
 </script>
 
 <template>
-  <div
-    role="alert" aria-busy="true"
-    :class="cn('v-spinner moon-animation', props.class)"
-    :style="wrapperStyles"
-  >
-    <span
-      class="moon-animation ball"
-      :style="ballStyles"
-    />
-    <span
-      class="circle"
-      :style="circleStyles"
-    />
-  </div>
+  <span :class="cn('vl-moon-spinner', $props.class as string)" :style="wrapper" role="progress" aria-busy="true">
+    <span :style="ball" />
+    <span :style="circle" />
+  </span>
 </template>
 
 <style>
-.moon-animation
-{
-    transform-origin: center;
-    -webkit-animation: v-moon-rotation 0.6s 0s infinite linear;
-            animation: v-moon-rotation 0.6s 0s infinite linear;
-    -webkit-animation-fill-mode: forwards;
-            animation-fill-mode: forwards;
-}
-
-@-webkit-keyframes v-moon-rotation
+@-webkit-keyframes vl-moon-rotation
 {
     to {
       transform: rotate(1turn);
    }
 }
 
-@keyframes v-moon-rotation
+@keyframes vl-moon-rotation
 {
     to {
       transform: rotate(1turn);
